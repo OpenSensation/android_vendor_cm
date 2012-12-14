e14941a223703a2f8d6ba449f493d3290868cb13
TamCore <philipp@tamcore.eu>
2012-12-14 21:36:27 +0100

    add ro.os.version

--- config/common.mk
+++ config/common.mk
@@ -228,7 +228,8 @@ endif
 
 PRODUCT_PROPERTY_OVERRIDES += \
   ro.cm.version=$(CM_VERSION) \
-  ro.modversion=$(CM_VERSION)
+  ro.modversion=$(CM_VERSION) \
+  ro.os.version=OpenSensation2-$(shell date +%Y%m%d)-$(CM_BUILD)
 
 
 -include $(WORKSPACE)/hudson/image-auto-bits.mk
